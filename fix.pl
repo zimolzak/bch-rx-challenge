dc22dd19cc0ea8b27693a05453d3b4a2db49763b
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-11 12:26:34 -0400

    diagnostics on two regexes

--- fix.pl
+++ fix.pl
@@ -7,5 +7,13 @@ while(<>){
     $firstpart =~ s/^([1234567890abcdef]+,\d*,).*/$1/;
     my $lastpart = $_;
     $lastpart =~ s/.*(,[N\/A0123456789]+,ND,.*)/$1/;
-    print $lastpart . "\n";
+
+    my @dm1 = split(/,/, $firstpart);
+    my @dm2 = split(/,/, $lastpart);
+    my $m = $#dm1;
+    $m++;
+    # print "$m \n";
+    my $n = $#dm2;
+    $n++;
+    print $lastpart . "\n"  if $n > 14;
 }
