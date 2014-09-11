0152cec066c055bfc0304a1689ba1d41f89dd319
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-11 13:03:06 -0400

    mostly working splitting of middle part

--- fix.pl
+++ fix.pl
@@ -6,14 +6,19 @@ while(<>){
     my $firstpart = $_;
     $firstpart =~ s/^([1234567890abcdef]+,\d*,).*/$1/;
     my $lastpart = $_;
-    $lastpart =~ s/.*(,[N\/A0123456789]+,ND,.*)/$1/;
+    $lastpart =~ s{.*(,[N/A0123456789]+,ND,.*)}{$1};
 
-    my @dm1 = split(/,/, $firstpart);
-    my @dm2 = split(/,/, $lastpart);
-    my $m = $#dm1;
-    $m++;
-    # print "$m \n";
-    my $n = $#dm2;
-    $n++;
-    print $lastpart . "\n"  if $n > 14;
+#     my @dm1 = split(/,/, $firstpart);
+#     my @dm2 = split(/,/, $lastpart);
+#     my $m = $#dm1;
+#     $m++;
+#     # print "$m \n";
+#     my $n = $#dm2;
+#     $n++;
+#     print $lastpart . "\n"  if $n > 14;
+
+    my $length_middle = length($_) - length($lastpart) - length($firstpart);
+    my $middlepart = substr($_, length($firstpart), $length_middle);
+    $middlepart =~ s/(.*),(\d*),(.*)/$1|$2|$3/;
+    
 }
