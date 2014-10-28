abef249701d669e4d22b65dd2607f9fe72f3c17e
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-10-28 05:40:08 -0400

    tidy up some

--- import.R
+++ import.R
@@ -1,35 +1,10 @@
 X = read.csv("~/Desktop/local/surescript-code/fixed_drug_details.csv", sep="|", quote="", colClasses=c("character", "integer", "character", "integer", "character", "character", "factor", "character", "numeric", "Date", "numeric", "factor", "numeric", "integer", "factor", "integer", "Date", "character"))
 
-
-# bad news
-apply(X[1,], 2, class) # all character
-
 q = c("x")
-for (i in 1:18) {
+n = dim(X)[2]
+for (i in 1:n) {
 	q = c(q, class(X[1,i]) )
 }
-rbind(names(X), q[2:19]) # all factor
+rbind(names(X), q[2:(n+1)])
 
-head(X$DrugCodedProductCodeQualifier) # something is definitely bad
-
-X = read.csv("~/Desktop/local/surescript-code/deleteme.txt", sep="|")
-
-
-X$messageid = as.character(X$messageid)
-X$rowid = as.numeric(as.character(X$rowid))
-X$DrugDescription = as.character(X$DrugDescription)
-X$DaySupply = as.numeric(as.character(X$DaySupply))
-X$Directions = as.character(X$Directions)
-X$DrugCodedProductCode = as.numeric(as.character(X$DrugCodedProductCode))
-# DrugCodedProductCodeQualifier # actually levels like ND
-X$DrugCodedStrength = as.character(X$DrugCodedStrength)
-X$FillNumber = as.numeric(as.character(X$FillNumber))
-# FillDate
-# Quantity # unused?
-# QuantityQualifier # I think always ZZ
-X$QuantityValue = as.numeric(as.character(X$QuantityValue))
-# Refills # unused?
-# RefillQualifier # actually levels
-X$RefillQuantity = as.numeric(as.character(X$RefillQuantity))
-# DateWritten
-X$NPI = as.character(X$NPI)
+#first two ones to look at are Quantity and Refills
