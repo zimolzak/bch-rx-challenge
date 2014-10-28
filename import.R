7f5df1064231d72c617f1827b94d6f2a44028c27
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-10-28 05:05:26 -0400

    ugly start to coercions

--- import.R
+++ import.R
@@ -8,3 +8,23 @@ for (i in 1:18) {
 }
 q[2:19] # all factor
 
+head(X$DrugCodedProductCodeQualifier) # something is definitely bad
+
+X$messageid = as.character(X$messageid)
+X$rowid = as.numeric(as.character(X$rowid))
+X$DrugDescription = as.character(X$DrugDescription)
+X$DaySupply = as.numeric(as.character(X$DaySupply))
+X$Directions = as.character(X$Directions)
+X$DrugCodedProductCode = as.numeric(as.character(X$DrugCodedProductCode))
+# DrugCodedProductCodeQualifier # actually levels like ND
+X$DrugCodedStrength = as.character(X$DrugCodedStrength)
+X$FillNumber = as.numeric(as.character(X$FillNumber))
+# FillDate
+# Quantity # unused?
+# QuantityQualifier # I think always ZZ
+X$QuantityValue = as.numeric(as.character(X$QuantityValue))
+# Refills # unused?
+# RefillQualifier # actually levels
+X$RefillQuantity = as.numeric(as.character(X$RefillQuantity))
+# DateWritten
+X$NPI = as.character(X$NPI)
