a69ae59e8ddae749e26bf5efa5889e8f79994464
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-11-15 08:21:39 -0500

    some analysis of delay between rx and fill

--- exploratory.R
+++ exploratory.R
@@ -10,12 +10,14 @@ joined = merge(x = drugDetails, y = medHxRecord, by = "messageid")
 sorted = joined[order(joined$patientid, joined$DrugCodedProductCode, joined$FillDate),]
 # 10 sec
 
+#### elementary counts
+
 patient_list = unique(sorted$patientid)
 length(patient_list) # 35190 patients
 drug_list = unique(sorted$DrugCodedProductCode)
 length(drug_list) # 16015 NDCs
 
-#####
+##### find most frequently prescribed NDCs
 
 row.names(sorted) = (1:dim(sorted)[1])
 
@@ -24,9 +26,6 @@ df_ndc = data.frame(ndc = sorted$DrugCodedProductCode, stringsAsFactors=FALSE)
 ndc_bestsellers = rle(sort(df_ndc$ndc)) # 15 sec
 ndc_best_df = data.frame(count = unlist(ndc_bestsellers[1]), DrugCodedProductCode = unlist(ndc_bestsellers[2]))
 ndc_best_df = ndc_best_df[order(ndc_best_df$count, decreasing = TRUE),]
-
-
-
 
 uniq_drug_rownames = row.names(unique(df_ndc))
 uniq_drugs = sorted[uniq_drug_rownames,] # about 45 sec of CPU-intensive
@@ -37,7 +36,21 @@ bestseller_w_names = bestseller_w_names[order(bestseller_w_names$count, decreasi
 
 head(bestseller_w_names, n=100)
 
-#uniq_drugs = uniq_drugs$DrugDescription
-#uniq_drugs = unique(uniq_drugs)
-#uniq_drugs = sort(uniq_drugs)
-#length(uniq_drugs)
+#### elementary analyses on DrugDescription
+
+length(unique(uniq_drugs$DrugDescription)) # 9451; not all NDCs have unique descriptions
+
+head(unique(sort(uniq_drugs$DrugDescription)),n=100)
+
+#### 
+delay = sorted[sorted$FillNumber==0,"FillDate"] - sorted[sorted$FillNumber==0,"DateWritten"]
+require(ggplot2)
+qplot(x=as.numeric(delay), log="x")
+
+plot = ggplot(data.frame(delay = as.numeric(delay)), aes(x=delay))
+plot + geom_density() + scale_x_log10()
+
+delay_all = data.frame(delay = as.numeric(sorted$FillDate - sorted$DateWritten))
+sorted = cbind(sorted,delay_all)
+
+# qplot(data=sorted, x=DateWritten, y=delay) # bad idea on 1E6 data points.
