59a772e74cfbd8ec808ea6cc2c8249907d172823
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-11-15 07:05:29 -0500

    some code to figure out most popular drugs - but working funny

--- exploratory.R
+++ exploratory.R
@@ -7,3 +7,34 @@ drugDetails = X
 joined = merge(x = drugDetails, y = medHxRecord, by = "messageid")
 # around a minute
 
+sorted = joined[order(joined$patientid, joined$DrugCodedProductCode, joined$FillDate),]
+# 10 sec
+
+patient_list = unique(sorted$patientid)
+length(patient_list) # 35190 patients
+drug_list = unique(sorted$DrugCodedProductCode)
+length(drug_list) # 16015 NDCs
+
+#####
+
+df_ndc = data.frame(ndc = sorted$DrugCodedProductCode, stringsAsFactors=FALSE)
+ndc_bestsellers = rle(sort(df_ndc$ndc)) # 15 sec
+
+ndc_best_df = data.frame(count = unlist(ndc_bestsellers[1]), DrugCodedProductCode = unlist(ndc_bestsellers[2]))
+ndc_best_df = ndc_best_df[order(ndc_best_df$count, decreasing = TRUE),]
+
+
+
+
+uniq_drug_rownames = row.names(unique(df_ndc))
+uniq_drugs = sorted[row.names(u),] # about 45 sec of CPU-intensive
+ndc_dict = uniq_drugs[c("DrugDescription", "DrugCodedProductCode")]
+
+bestseller_w_names = merge(x=ndc_dict, y=ndc_best_df, by="DrugCodedProductCode")
+bestseller_w_names = bestseller_w_names[order(bestseller_w_names$count, decreasing=TRUE),]
+bestseller_w_names = unique(bestseller_w_names)
+
+#uniq_drugs = uniq_drugs$DrugDescription
+#uniq_drugs = unique(uniq_drugs)
+#uniq_drugs = sort(uniq_drugs)
+#length(uniq_drugs)
